4303caff2900296702d0c4c9140c965d2daf041e
Fabian Wenzelmann <fabianwenzelmann@posteo.de>
2018-10-05 05:14:00 +0200

    added copyright command

--- cmd/mosaic/mosaic.go
+++ cmd/mosaic/mosaic.go
@@ -129,6 +129,13 @@ func init() {
 		Usage:       "help",
 		Description: "Print this help message",
 	}
+
+	// add copyright command
+	cmdMap["copyright"] = gomosaic.Command{
+		Exec:        copyrightCommand,
+		Usage:       "copyright",
+		Description: "Show copyright information",
+	}
 }
 
 func exitCommand(state *gomosaic.ExecutorState, args ...string) error {
@@ -147,6 +154,24 @@ func helpCommand(state *gomosaic.ExecutorState, args ...string) error {
 		fmt.Println("Usage:", cmd.Usage)
 		fmt.Println(cmd.Description)
 	}
+	return nil
+}
+
+func copyrightCommand(state *gomosaic.ExecutorState, args ...string) error {
+	license := `Copyright 2018 Fabian Wenzelmann
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`
+	fmt.Println(license)
 	return nil
 }
 
